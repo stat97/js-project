413269608c4abc5fd9015ed810fac88e08132734
Adrian <ortizdev@proton.me>
2024-01-26 18:33:56 +0100

    CardPokemon

--- hubjuegos/src/components/index.js
+++ hubjuegos/src/components/index.js
@@ -1,3 +1,4 @@
-// index.js------ src/componets/index.js
+// index.js-----> src/components/index.js
 export * from "./Header/Header";
 export * from "./Footer/Footer";
+export * from "./CardPokemons/CardPokemons";
